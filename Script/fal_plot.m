c966580e417f4afe1a618b628ff1b553637be75d
Risky Jr <2114768901@qq.com>
2020-05-18 20:32:34 +0800

    修改fal函数

--- Script/fal_plot.m
+++ Script/fal_plot.m
@@ -10,12 +10,17 @@ catch
 end
 x = -1:0.01:1;
 y = zeros(0);
-for i = 1:length(x)
-    y(i) = fal(x(i), 2, 0.1 );
+lineLable = cell(0);
+for alpha = 0:1:10
+    for i = 1:length(x)
+        y(i) = fal(x(i), alpha, 0.1 );
+    end
+    plot(x,y,'LineWidth',alpha*0.2 + 0.5);
+    lineLable{alpha+1} = strcat('alpha=',num2str(alpha+1));
+    hold on
 end
-plot(x,y);
-hold on
 plot(x,x)
+legend(lineLable)
 grid on
 
 %% fal
